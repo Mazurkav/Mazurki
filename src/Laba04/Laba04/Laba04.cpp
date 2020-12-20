183007ba9be26d809cd7450dc8e6c491663eb6c7
Mazurkav <73746284+Mazurkav@users.noreply.github.com>
2020-12-20 18:50:50 +0200

    Add files via upload

---
+++ src/Laba04/Laba04/Laba04.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std; 
+
+
+int main() {
+
+    int number;
+
+    cout << "Input number:" << endl;
+    cin >> number;
+
+
+    if (number >= 100 && number <= 999) 
+    {
+        if ((number / 100 == (number / 10) % 10) && (number / 100 == number % 100 % 10)) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+    else {
+        cout << "Invalid value" << endl;
+    }
+
+
+    return 0;
+}
+
+
---
+++ src/Laba05/Laba05/Laba05.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    float  val = 987321;
+    long ipart = (long)val;
+    unsigned long fpart = 0;
+    long lvalue = 0;
+    val = val - ipart;
+    while (val)
+    {
+        val *= 10;
+        lvalue = (long)val;
+        fpart *= 10;
+        fpart = fpart + lvalue;
+        val = val - lvalue;
+    }
+    while (fpart)
+    {
+        cout << fpart % 10;
+        fpart /= 10;
+    }
+    cout << ".";
+    while (ipart)
+    {
+        cout << ipart % 10;
+        ipart /= 10;
+    }
+    return 0;
+}
